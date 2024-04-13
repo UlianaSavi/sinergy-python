45f03a5bb791fd1b1a3f13bb56ea9d8083277e69
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-14 02:56:07 +0600

    improve calc

--- src/get_rectangle_Info.py
+++ src/get_rectangle_Info.py
@@ -9,30 +9,29 @@ class GetRectangleInfo:
      def __init__(self):
          print("\033[32mПривет! Я калькулятор площади и периметра прямоугольника.\nВведите стороны прямоугольника (в см) для произведения вычислений:\n")
          self.ask()
-         print("\n\033[32mИтааак, результат! Площадь прямоугольника: {}. Периметр прямоугольника: {}.".format(self.square, self.perimeter))
+         print("\n\033[32mИтааак, результат! Площадь прямоугольника: {} см. Периметр прямоугольника: {} см.".format(self.square, self.perimeter))
          print("\033[32mСпасибо за использование! Досвидания!\n")
          sys.exit()
 
      def ask(self):
       try:
-        inputRes = input("\033[35mСколько сантиметров сторона {} ?\n Ваш ответ: ".format(self.sides[self.activeSide]))
-        if inputRes & (isinstance(inputRes, float) | isinstance(inputRes, int)): # проверяем является ли ввод числом
-           self.sides[self.activeSide] = inputRes
-           self.activeSide += "b"
-           if self.activeSide != "b":
-              self.ask()
-           else:
-              square = self.getSquare()
-              perimeter = self.getPerimeter()
-              self.square = square
-              self.perimeter = perimeter
-        else:
-           print("\n\033[33mВведено не числовое значение, пожалуйста повторите ввод.\n")
+        inputRes = input("\033[35mСколько сантиметров сторона {} ?\n Ваш ответ: ".format(self.activeSide))
+        inputRes = int(inputRes)
+        self.sides[self.activeSide] = inputRes
+        if self.activeSide == "a":
+           self.activeSide = "b"
            self.ask()
-
+        else:
+           square = self.getSquare()
+           perimeter = self.getPerimeter()
+           self.square = square
+           self.perimeter = perimeter
       except KeyboardInterrupt:
          print("\n\033[33mВы вышли из калькулятора. Досвидания!\n")
          sys.exit()
+      except ValueError:
+         print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
+         self.ask()
       except:
          print("\n\033[33mВы вышли из калькулятора. Досвидания!\n")
          sys.exit()
@@ -42,7 +41,7 @@ class GetRectangleInfo:
          return square
 
      def getPerimeter(self):
-         perimeter = (self.sides["a"] + self.sides["b"])**2
+         perimeter = (self.sides["a"])*2 + (self.sides["b"])*2
          return perimeter
 
 
