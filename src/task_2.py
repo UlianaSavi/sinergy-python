d8abf2e9ff4bba09fa992e55480bcf5595a1594f
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-16 15:08:55 +0600

    add solution for task 2

--- src/task_2.py
+++ src/task_2.py
@@ -1,25 +1,24 @@
 import sys
 
 
-class countZeros:
-    answer = []
+class countDivisor:
+    answer = 2  # 1 и само число
 
     def __init__(self):
         print("\033[32mПривет!\n")
         self.ask()
-        print("\n\033[32mВ введенных вами числах нулевых: {}".format(len(self.answer)))
+        print(
+            "\n\033[32mКоличество натуральных делителей числа X: {}".format(self.answer)
+        )
         print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
         sys.exit()
 
     def ask(self):
         try:
-            num = int(input("\n\033[32mВведите целое число N: "))
-            i = 0
-            while i < num:
-                i += 1
-                itemNum = input("\n\033[32mВведите целое число {}: ".format(i))
-                if int(itemNum) == 0:
-                    self.answer.append(int(itemNum))
+            x = int(input("\n\033[32mВведите натуральное число X: "))
+            for i in range(2, int(x / 2) + 1):
+                if x % i == 0:
+                    self.answer += 1
         except KeyboardInterrupt:
             print(
                 "\n\033[33mВы отказались пройти оценку возможностей инвестирования. Досвидания!\n"
@@ -35,4 +34,4 @@ class countZeros:
             sys.exit()
 
 
-newEx = countZeros()
+newEx = countDivisor()
