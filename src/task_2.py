334b63e53c35584914e642f82c1cc2ae0b80487f
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-16 19:17:38 +0600

    add solution for second task

--- src/task_2.py
+++ src/task_2.py
@@ -2,12 +2,15 @@
 import sys
 
 
-class ClassName:
+class DeleteSpaces:
     answer = ""
     inputNum = 0
+    maxInputLen = 1000
 
     def __init__(self):
-        print("\033[32mПривет!\n")
+        print(
+            "\033[32mПривет! Этот скрипт удалит все лишние пробелы из строки. Максимальная длина строки 1000 символов.\n"
+        )
         self.ask()
         print("\n\033[32mРезультат: {}".format(self.answer))
         print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
@@ -15,14 +18,21 @@ class ClassName:
 
     def ask(self):
         try:
-            inputRes = input("\033[35mВопрос ")
-            print(inputRes)
+            inputRes = input("\033[35mВведите строку для преобразования: ")
+            if len(inputRes) > self.maxInputLen:
+                raise ValueError("ValueError")
+            self.answer = " ".join(inputRes.split())
+        except ValueError:
+            print(
+                "\n\033[33mВы ввели неверные значения (строка превышает 1000 символов), попробуйте снова!\n"
+            )
+            self.ask()
         except KeyboardInterrupt:
-            print("\n\033[33mВы отказались пройти тест. Досвидания!\n")
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
             sys.exit()
         except:
-            print("\n\033[33mВы отказались пройти тест. Досвидания!\n")
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
             sys.exit()
 
 
-newEx = ClassName()
+newEx = DeleteSpaces()
