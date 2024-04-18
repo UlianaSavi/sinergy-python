472d3bc477723ada6643ed4c9c0e05afede8f37d
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-18 13:45:54 +0600

    add solution for 1st task

--- src/task_1.py
+++ src/task_1.py
@@ -2,7 +2,8 @@ import sys
 
 
 class ClassName:
-    answer = ""
+    answer = 0
+    maxNumbers = 100000
 
     def __init__(self):
         print("\033[32mПривет!\n")
@@ -13,8 +14,42 @@ class ClassName:
 
     def ask(self):
         try:
-            inputRes = input("\033[35mВопрос ")
-            self.answer = inputRes
+            numbersLen = int(
+                input(
+                    "\033[35mВведите число N – количество чисел (1 ≤ N ≤ {}) ".format(
+                        self.maxNumbers
+                    )
+                )
+            )
+            if numbersLen <= 0 or numbersLen > self.maxNumbers:
+                raise ValueError
+
+            arr = list(
+                map(
+                    int,
+                    input(
+                        "Введите {} чисел через пробел (каждое число не превышает 2*10e9 по модулю)".format(
+                            numbersLen
+                        )
+                    ).split(),
+                )
+            )
+            for numItem in arr:
+                if numItem > 2 * 10e9:
+                    raise ValueError
+            if len(arr) != numbersLen:
+                raise ValueError
+            res = []
+            for item in arr:
+                if item not in res:
+                    res.append(item)
+
+            self.answer = len(res)
+        except ValueError:
+            print(
+                "\n\033[33mВы ввели неверное значение или неверное кол-во значений, попробуйте снова!\n"
+            )
+            self.ask()
         except KeyboardInterrupt:
             print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
             sys.exit()
