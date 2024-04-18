f48ac41eaf40dddac162226ce320f35faf0d1fbd
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-18 15:07:42 +0600

    add solution for 3st task

--- src/task_3.py
+++ src/task_3.py
@@ -2,19 +2,36 @@ import sys
 
 
 class ClassName:
-    answer = ""
+    answer = {}
 
     def __init__(self):
         print("\033[32mПривет!\n")
         self.ask()
-        print("\n\033[32mРезультат: {}".format(self.answer))
+        for key in self.answer:
+            print("\036 {}: {}".format(key, self.answer[key]))
         print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
         sys.exit()
 
     def ask(self):
         try:
-            inputRes = input("\033[35mВопрос ")
-            self.answer = inputRes
+            arr = list(
+                map(
+                    int,
+                    input(
+                        "\033[35mВведите последовательность чисел через пробел: "
+                    ).split(),
+                )
+            )
+            res = []
+            for item in arr:
+                if item not in res:
+                    res.append(item)
+                    self.answer[item] = "NO"
+                else:
+                    self.answer[item] = "YES"
+        except ValueError:
+            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
+            self.ask()
         except KeyboardInterrupt:
             print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
             sys.exit()
