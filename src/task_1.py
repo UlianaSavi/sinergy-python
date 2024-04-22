08a21f016105769830b784de2ff397b06d2608b0
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-22 14:28:17 +0600

    fix getting age suffix bug

--- src/task_1.py
+++ src/task_1.py
@@ -12,7 +12,7 @@ class Poll:
         ]  # используется чисто в рамках требования по тз задания. Получает единственного питомца из списка pets
         print(
             "Это {} по кличке {}. Возраст питомца: {}. Имя владельца: {}".format(
-                pet["breed"], keys[0], self.getAgeStr(pet["age"]), pet["ownerName"]
+                pet["breed"], keys[0], self.getAgeSuffix(pet["age"]), pet["ownerName"]
             )
         )
         print("\033[32mСпасибо за прохождение опроса! Досвидания!\n")
@@ -40,16 +40,27 @@ class Poll:
             print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
             sys.exit()
 
-    def getAgeStr(self, age=0):
+    def getAgeSuffix(self, age=0):
         ageTypes = ["год", "года", "лет"]
         currAgeType = ageTypes[0]
         lastDigit = int(repr(age)[-1])  # смотрим последнюю цифру числа
-        if lastDigit == 1:
-            currAgeType = ageTypes[0]
-        if lastDigit == 2 or lastDigit == 3 or lastDigit == 4:
-            currAgeType = ageTypes[1]
-        if lastDigit == 0 or lastDigit == 5:
-            currAgeType = ageTypes[2]
+        if age > 20:
+            if lastDigit == 1:
+                currAgeType = ageTypes[0]
+            if lastDigit == 2 or lastDigit == 3 or lastDigit == 4:
+                currAgeType = ageTypes[1]
+            if lastDigit == 0 or lastDigit == 5:
+                currAgeType = ageTypes[2]
+            if lastDigit >= 5 and lastDigit <= 9:
+                currAgeType = ageTypes[2]
+        else:
+            if age == 1:
+                currAgeType = ageTypes[0]
+            if age >= 2 and age < 5:
+                currAgeType = ageTypes[1]
+            if age >= 5 and age <= 20:
+                currAgeType = ageTypes[2]
+        print(str(age) + " " + currAgeType)
         return str(age) + " " + currAgeType
 
 
