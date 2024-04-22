22747da5d823565ca36b4a788bf785b46fcc6ee2
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-22 14:27:47 +0600

    fix getting age suffix bug

--- src/task_2.py
+++ src/task_2.py
@@ -22,55 +22,54 @@ class VetClinic:
 
     def __init__(self):
         print("\033[32mПривет!\n")
-        self.ask()
-        print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
+        self.getAgeSuffix(28)
+        self.getAgeSuffix(21)
+        self.getAgeSuffix(13)
+        self.getAgeSuffix(4)
+        # self.ask()
+        # print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
         sys.exit()
 
     def ask(self):
-        try:
-            commands = ["create", "read", "update", "delete", "stop"]
-            command = commands[0]
-            while command != commands[4]:  # stop
-                inputRes = input(
-                    "\033[35mВведите одну из команд для взаимодействия с базой - create, read, update, delete: "
+        commands = ["create", "read", "update", "delete", "stop"]
+        command = commands[0]
+        while command != commands[4]:  # stop
+            inputRes = input(
+                "\033[35mВведите одну из команд для взаимодействия с базой - create, read, update, delete: "
+            )
+            command = inputRes
+            if command == commands[0]:
+                self.create(self.inputDataForNewItem())
+            if command == commands[1]:
+                name = input(
+                    "\033[35mВведите кличку питомца для получения информации: "
                 )
-                command = inputRes
-                if command == commands[0]:
-                    self.create(self.inputDataForNewItem())
-                if command == commands[1]:
-                    name = input(
-                        "\033[35mВведите кличку питомца для получения информации: "
-                    )
-                    self.read(name)
-                if command == commands[2]:
-                    name = input(
-                        "\033[35mВведите кличку питомца для обновления информации: "
-                    )
-                    self.update(name, self.inputDataForNewItem(name))
-                if command == commands[3]:
-                    name = input(
-                        "\033[35mВведите кличку питомца для удаления информации о нем: "
-                    )
-                    self.delete(name)
-        except ValueError:
-            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
-            self.ask()
-        except KeyboardInterrupt:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
-        except:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
+                self.read(name)
+            if command == commands[2]:
+                name = input(
+                    "\033[35mВведите кличку питомца для обновления информации: "
+                )
+                self.update(name, self.inputDataForNewItem(name))
+            if command == commands[3]:
+                name = input(
+                    "\033[35mВведите кличку питомца для удаления информации о нем: "
+                )
+                self.delete(name)
 
     def create(self, newPet={}):
         last = collections.deque(self.pets, maxlen=1)[0]  # последний ключ
         self.pets[last + 1] = newPet
 
     def read(self, name=""):
-        pet = self.pets[name]
+        arr = list(self.pets.values())
+        res = ""
+        print(arr)
+        for i in arr:
+            if name == list(i.keys())[0]:
+                res = list(i.values())[0]
         print(
-            "Это {} по кличке {}. age: {}. ownerName: {}".format(
-                pet["breed"], name, self.getAgeSuffix(pet["age"]), pet["ownerName"]
+            "Это {} по кличке {}. Возраст питомца: {}. Имя владельца: {}".format(
+                res["breed"], name, self.getAgeSuffix(res["age"]), res["ownerName"]
             )
         )
 
@@ -89,12 +88,23 @@ class VetClinic:
         ageTypes = ["год", "года", "лет"]
         currAgeType = ageTypes[0]
         lastDigit = int(repr(age)[-1])  # смотрим последнюю цифру числа
-        if lastDigit == 1:
-            currAgeType = ageTypes[0]
-        if lastDigit == 2 or lastDigit == 3 or lastDigit == 4:
-            currAgeType = ageTypes[1]
-        if lastDigit == 0 or lastDigit == 5:
-            currAgeType = ageTypes[2]
+        if age > 20:
+            if lastDigit == 1:
+                currAgeType = ageTypes[0]
+            if lastDigit == 2 or lastDigit == 3 or lastDigit == 4:
+                currAgeType = ageTypes[1]
+            if lastDigit == 0 or lastDigit == 5:
+                currAgeType = ageTypes[2]
+            if lastDigit >= 5 and lastDigit <= 9:
+                currAgeType = ageTypes[2]
+        else:
+            if age == 1:
+                currAgeType = ageTypes[0]
+            if age >= 2 and age < 5:
+                currAgeType = ageTypes[1]
+            if age >= 5 and age <= 20:
+                currAgeType = ageTypes[2]
+        print(str(age) + " " + currAgeType)
         return str(age) + " " + currAgeType
 
     def inputDataForNewItem(name=False):
