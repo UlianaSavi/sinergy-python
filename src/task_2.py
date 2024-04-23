60237bdf377553e7036279421ca053dbbd87a7d8
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-23 14:57:45 +0600

    fix solution for 2nd task

--- src/task_2.py
+++ src/task_2.py
@@ -9,51 +9,42 @@ class GetListsCount:
         print("\033[32mПривет!\n")
         self.ask()
         print(
-            "\n\033[32m{} чисел содержится одновременно как в первом списке, так и во втором.".format(
-                self.answer
-            )
+            "\n\033[32mКоличество чисел в пересечении множеств: {}".format(self.answer)
         )
         print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
         sys.exit()
 
     def ask(self):
-        try:
-            list1 = []
-            list2 = []
-            endedOne = False
-            endedTwo = True
-            while endedOne == False:
-                list1Item = input(
-                    "\033[35mВведите целые числа ПЕРВОГО списка (чтобы закончить ввод напишите end)). Максимальное кол-во чисел - {} ".format(
-                        self.maxListLen
-                    ),
-                )
-                if list1Item == "end":
-                    endedOne = True
-                    endedTwo = False
-                    print("\n")
-                else:
-                    list1.append(int(list1Item))
-            while endedTwo == False:
-                list2Item = input(
-                    "\033[35mВведите целые числа ВТОРОГО списка (чтобы закончить ввод напишите end)). Максимальное кол-во чисел - {} ".format(
-                        self.maxListLen
-                    ),
-                )
-                if list2Item == "end":
-                    endedTwo = True
-                else:
-                    list2.append(int(list2Item))
-                self.answer = len(list1) + len(list2)
-        except KeyboardInterrupt:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
-        except ValueError:
-            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
-            self.ask()
-        except:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
+        # try:
+        list1 = set()
+        list2 = set()
+        list1Item = input(
+            "\033[35mВведите целые числа ПЕРВОГО списка через пробел. Максимальное кол-во чисел - {} ".format(
+                self.maxListLen
+            ),
+        )
+        arr1 = map(int, list1Item.split())
+        list1 = set(arr1)
+
+        list2Item = input(
+            "\033[35mВведите целые числа ВТОРОГО списка через пробел. Максимальное кол-во чисел - {} ".format(
+                self.maxListLen
+            ),
+        )
+        arr2 = map(int, list2Item.split())
+        list2 = set(arr2)
+        intersection = list1 & list2
+        self.answer = len(intersection)
+
+    # except KeyboardInterrupt:
+    #     print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+    #     sys.exit()
+    # except ValueError:
+    #     print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
+    #     self.ask()
+    # except:
+    #     print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+    #     sys.exit()
 
 
 newEx = GetListsCount()
