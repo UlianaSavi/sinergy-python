398fadb0194d6b7cf0fec863f6a8d70e05ddfe53
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-24 15:29:28 +0600

    return try&except

--- src/generateMatrix.py
+++ src/generateMatrix.py
@@ -16,32 +16,32 @@ class GenerateMatrix:
         sys.exit()
 
     def ask(self):
-        # try:
-        columnSize = int(input("\033[35mВведите размер матриц по колонкам: "))
-        rowSize = int(input("\033[35mВведите размер матриц по строкам:  "))
-        if bool(columnSize) and bool(rowSize):
-            self.matrixA = self.generateMatrix(columnSize, rowSize)
-            self.matrixB = self.generateMatrix(columnSize, rowSize)
-        else:
-            self.matrixA = self.generateMatrix()
-            self.matrixB = self.generateMatrix()
-        print("\033[32mГенерирую первую матрицу...")
-        print("\033[32mРезультат: {}\n".format(self.matrixA))
-        print("\033[32mГенерирую вторую матрицу...")
-        print("\033[32mРезультат: {}\n".format(self.matrixB))
-        print("\033[32mВыполняю сложение двух матриц...")
+        try:
+            columnSize = int(input("\033[35mВведите размер матриц по колонкам: "))
+            rowSize = int(input("\033[35mВведите размер матриц по строкам:  "))
+            if bool(columnSize) and bool(rowSize):
+                self.matrixA = self.generateMatrix(columnSize, rowSize)
+                self.matrixB = self.generateMatrix(columnSize, rowSize)
+            else:
+                self.matrixA = self.generateMatrix()
+                self.matrixB = self.generateMatrix()
+            print("\033[32mГенерирую первую матрицу...")
+            print("\033[32mРезультат: {}\n".format(self.matrixA))
+            print("\033[32mГенерирую вторую матрицу...")
+            print("\033[32mРезультат: {}\n".format(self.matrixB))
+            print("\033[32mВыполняю сложение двух матриц...")
 
-        self.getMatrixSum(self.matrixA, self.matrixB)
+            self.getMatrixSum(self.matrixA, self.matrixB)
 
-    # except ValueError:
-    #     print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
-    #     self.ask()
-    # except KeyboardInterrupt:
-    #     print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-    #     sys.exit()
-    # except:
-    #     print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-    #     sys.exit()
+        except ValueError:
+            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
+            self.ask()
+        except KeyboardInterrupt:
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+            sys.exit()
+        except:
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+            sys.exit()
 
     def generateMatrix(self, columnSize=defaultMatrixSize, rowSize=defaultMatrixSize):
         return [
