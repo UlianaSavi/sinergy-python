7e53bf795bea19c477ee2500de51e9e2a199164a
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-24 16:20:39 +0600

    add template for tasks && update readme

---
+++ src/task_2.py
@@ -0,0 +1,29 @@
+import sys
+
+
+class ClassName:
+    answer = ""
+
+    def __init__(self):
+        print("\033[32mПривет!\n")
+        self.ask()
+        print("\n\033[32mРезультат: {}".format(self.answer))
+        print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
+        sys.exit()
+
+    def ask(self):
+        try:
+            inputRes = input("\033[35mВопрос ")
+            self.answer = inputRes
+        except ValueError:
+            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
+            self.ask()
+        except KeyboardInterrupt:
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+            sys.exit()
+        except:
+            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
+            sys.exit()
+
+
+newEx = ClassName()
