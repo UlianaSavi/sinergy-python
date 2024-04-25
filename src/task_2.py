825b3caf52149ec04b8e2d4688bfcf137e00ba3d
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-04-25 13:16:07 +0600

    add solution for second task

--- src/task_2.py
+++ src/task_2.py
@@ -1,29 +1,44 @@
-import sys
+import math
 
 
-class ClassName:
-    answer = ""
+class Turtle:
 
     def __init__(self):
-        print("\033[32mПривет!\n")
-        self.ask()
-        print("\n\033[32mРезультат: {}".format(self.answer))
-        print("\033[32mСпасибо за использование скрипта! Досвидания!\n")
-        sys.exit()
+        self.x = 0
+        self.y = 0
+        self.s = 2
 
-    def ask(self):
-        try:
-            inputRes = input("\033[35mВопрос ")
-            self.answer = inputRes
-        except ValueError:
-            print("\n\033[33mВы ввели неверное значение, попробуйте снова!\n")
-            self.ask()
-        except KeyboardInterrupt:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
-        except:
-            print("\n\033[33mВы вышли из скрипта. Досвидания!\n")
-            sys.exit()
+    def goUp(self):
+        self.y = self.y + self.s
+
+    def goDown(self):
+        self.y = self.y - self.s
+
+    def goLeft(self):
+        self.x = self.x - self.s
+
+    def goRight(self):
+        self.x = self.x + self.s
+
+    def evolve(self):
+        self.s = self.s + 1
+
+    def degrade(self):
+        num = self.s - 1
+        if num <= 0:
+            raise ValueError(
+                f"s не может быть меньше или равна нулю! Сейчас s = {self.s}"
+            )
+        self.s = num
+
+    def countMoves(self, x2, y2):
+        num = math.ceil(abs(x2 - self.x) / self.s + abs(y2 - self.y) / self.s)
+        print(f"Черепашка может добраться до {x2};{y2}; за {num} ходов")
+        return num
 
 
-newEx = ClassName()
+newEx = Turtle()
+
+newEx.countMoves(4, 4)
+newEx.countMoves(2, 10)
+newEx.countMoves(2.3, 10)
